250dc1a704bc7c8df4bb7c9c82179d6fd87a8b17
baremaximum <therealcharlesdesbiens@gmail.com>
2021-03-28 19:39:33 -0400

    ready to try deploying

--- template/rust-actix-web/main/src/main.rs
+++ template/rust-actix-web/main/src/main.rs
@@ -1,9 +1,9 @@
 use actix_web::{App, HttpServer};
-use handler;
+use function;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(handler::handler))
+    HttpServer::new(|| App::new().service(function::handler))
         .bind("127.0.0.1:3000")?
         .run()
         .await
--- test/test_build_and_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Building function image..."
-
-docker build -t rust-actix-web-template-test -f "../template/Dockerfile"
-
-echo "test image build successful."
