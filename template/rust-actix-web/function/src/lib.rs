977af7e0d451d5f28ec942723b54ed0fc98b0403
baremaximum <therealcharlesdesbiens@gmail.com>
2021-03-29 03:42:06 -0400

    updated dockerfile and changed method to post

--- template/rust-actix-web/function/src/lib.rs
+++ template/rust-actix-web/function/src/lib.rs
@@ -1,18 +1,20 @@
-use actix_web::{get, HttpRequest, Responder};
+use actix_web::{post, HttpRequest, Responder};
 
-#[get("/")]
+#[post("/")]
 pub async fn handler(_req: HttpRequest) -> impl Responder {
     "OK"
 }
 
 mod tests {
+    #[allow(unused_imports)]
     use super::*;
+    #[allow(unused_imports)]
     use actix_web::{rt as actix_rt, test, web, App};
     #[actix_rt::test]
     async fn test_handler() {
         let mut app = test::init_service(App::new().service(handler)).await;
         // Change HTTP method below if required
-        let req = test::TestRequest::get().uri("/").to_request();
+        let req = test::TestRequest::post().uri("/").to_request();
         let response = test::call_service(&mut app, req).await;
         assert!(response.status().is_success());
     }
