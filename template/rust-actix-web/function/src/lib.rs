7e829113ee670155c317f99c0502786279235af8
baremaximum <therealcharlesdesbiens@gmail.com>
2021-04-24 18:43:48 -0400

    reworked to have main app in handler.

--- template/rust-actix-web/function/src/lib.rs
+++ template/rust-actix-web/function/src/lib.rs
@@ -1,15 +1,56 @@
-use actix_web::{post, HttpRequest, Responder};
+use actix_web::{middleware, post, web, App, HttpRequest, HttpServer, Responder};
+use log::info;
+use std::env;
 
 #[post("/")]
 pub async fn handler(_req: HttpRequest) -> impl Responder {
     "OK"
 }
 
+#[actix_web::main]
+pub async fn app_init() -> std::io::Result<()> {
+    // get worker pool size from env.
+    let cnt = env::var("WORKER_POOL_SIZE");
+    let mut worker_count: usize = 1;
+
+    match cnt {
+        Ok(cnt) => {
+            worker_count = cnt
+                .parse::<usize>()
+                .expect("Could not parse WORKER_POOL_SIZE. Value must parse to valid usize")
+        }
+        Err(_) => info!("WORKER_POOL_SIZE not set. Using default value 1."),
+    }
+
+    // get max json size from env.
+    let max = env::var("JSON_MAX_SIZE");
+    let mut max_size: usize = 4096;
+
+    match max {
+        Ok(max) => {
+            max_size = max
+                .parse::<usize>()
+                .expect("Could not parse WORKER_POOL_SIZE. Value must parse to valid usize")
+        }
+        Err(_) => info!("JSON_MAX_SIZE not set. Using default value 4096."),
+    }
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .data(web::JsonConfig::default().limit(max_size))
+            .service(handler)
+    })
+    .workers(worker_count)
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
+}
 mod tests {
     #[allow(unused_imports)]
     use super::*;
     #[allow(unused_imports)]
-    use actix_web::{rt as actix_rt, test, web, App};
+    use actix_web::{rt as actix_rt, test};
     #[actix_rt::test]
     async fn test_handler() {
         let mut app = test::init_service(App::new().service(handler)).await;
--- template/rust-actix-web/main/src/main.rs
+++ template/rust-actix-web/main/src/main.rs
@@ -1,43 +1,7 @@
-use actix_web::{web, middleware, App, HttpServer};
-use log::info;
 use function;
-use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
-
-    // get worker pool size from env.
-    let cnt = env::var("WORKER_POOL_SIZE");
-    let mut worker_count: usize = 1;
-
-    match cnt {
-        Ok(cnt) => { 
-            worker_count = cnt.parse::<usize>()
-                .expect("Could not parse WORKER_POOL_SIZE. Value must parse to valid usize") 
-        }
-        Err(_) => info!("WORKER_POOL_SIZE not set. Using default value 1.")
-    }
-
-    // get max json size from env.
-    let max = env::var("JSON_MAX_SIZE");
-    let mut max_size: usize = 4096;
-
-    match max {
-        Ok(max) => { 
-            max_size = max.parse::<usize>()
-                .expect("Could not parse WORKER_POOL_SIZE. Value must parse to valid usize") 
-        }
-        Err(_) => info!("JSON_MAX_SIZE not set. Using default value 4096.")
-    }
-
-    HttpServer::new(move || 
-        App::new()
-        .wrap(middleware::Logger::default())
-        .data(web::JsonConfig::default().limit(max_size))
-        .service(function::handler))
-        .workers(worker_count)
-        .bind("127.0.0.1:3000")?
-        .run()
-        .await
+    function::app_init()
 }
